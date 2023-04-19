5186ebb695d2c976dee30365128599ef1f58d8f8
David Khan <happygru237@gmail.com>
2023-04-19 16:08:27 -0400

    fix: eslint error fix

--- src/components/BadgeTextComponent/index.js
+++ src/components/BadgeTextComponent/index.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import { Badge } from 'primereact/badge'
-import './style.css'
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import { Badge } from "primereact/badge";
+import "./style.css";
 
 const BadgeText = ({ text, value, type }) => {
   return (
-    <div className='py-1 badgeTextItem'>
+    <div className="py-1 badgeTextItem">
       <Badge value={value} severity={type} />
-      <a className='pl-2'>{text}</a>
+      <a className="pl-2">{text}</a>
     </div>
-  )
-}
+  );
+};
 
-export default BadgeText
+export default BadgeText;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,54 +1,26 @@
-import React from 'react'
-import { Image } from 'primereact/image'
-import { Button } from 'primereact/button'
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import { Image } from "primereact/image";
+import { Button } from "primereact/button";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 
 const Header = () => {
-
-  const items = [
-    {
-      label: 'Videos', icon: 'pi pi-fw pi-video',
-      items: [
-        [
-            {
-                label: 'Video 1',
-                items: [{ label: 'Video 1.1' }, { label: 'Video 1.2' }]
-            },
-            {
-                label: 'Video 2',
-                items: [{ label: 'Video 2.1' }, { label: 'Video 2.2' }]
-            }
-        ],
-        [
-            {
-                label: 'Video 3',
-                items: [{ label: 'Video 3.1' }, { label: 'Video 3.2' }]
-            },
-            {
-                label: 'Video 4',
-                items: [{ label: 'Video 4.1' }, { label: 'Video 4.2' }]
-            }
-        ]
-      ]
-    }
-  ];
-
   return (
     <>
-      <header className='w-full bg-white relative z-10'>
-        <div className='container m-auto flex items-center justify-between'>
+      <header className="w-full bg-white relative z-10">
+        <div className="container m-auto flex items-center justify-between">
           <div>
             <Image src="logo.png" width="58" />
           </div>
           <div>
-            <ul className='list-none'>
-              <li className='inline'>
+            <ul className="list-none">
+              <li className="inline">
                 <div className="dropdown">
                   <button className="dropbtn">
                     <span>Products</span>
-                    <i className='pi pi-chevron-down'></i>
+                    <i className="pi pi-chevron-down"></i>
                   </button>
                   <div className="dropdown-content">
                     <a>Link 1</a>
@@ -57,11 +29,11 @@ const Header = () => {
                   </div>
                 </div>
               </li>
-              <li className='inline'>
+              <li className="inline">
                 <div className="dropdown">
                   <button className="dropbtn">
                     <span>Docs</span>
-                    <i className='pi pi-chevron-down'></i>
+                    <i className="pi pi-chevron-down"></i>
                   </button>
                   <div className="dropdown-content">
                     <a>Link 1</a>
@@ -70,11 +42,11 @@ const Header = () => {
                   </div>
                 </div>
               </li>
-              <li className='inline'>
+              <li className="inline">
                 <div className="dropdown">
                   <button className="dropbtn">
                     <span>Company</span>
-                    <i className='pi pi-chevron-down'></i>
+                    <i className="pi pi-chevron-down"></i>
                   </button>
                   <div className="dropdown-content">
                     <a>Link 1</a>
@@ -83,14 +55,14 @@ const Header = () => {
                   </div>
                 </div>
               </li>
-              <li className='inline'>
+              <li className="inline">
                 <div className="dropdown">
                   <button className="dropbtn">
                     <span>Pricing</span>
                   </button>
                 </div>
               </li>
-              <li className='inline'>
+              <li className="inline">
                 <div className="dropdown">
                   <button className="dropbtn">
                     <span>Contact</span>
@@ -99,14 +71,19 @@ const Header = () => {
               </li>
             </ul>
           </div>
-          <div className='signin'>
-            <Button label=" Login " severity="secondary" size="small" outlined />
+          <div className="signin">
+            <Button
+              label=" Login "
+              severity="secondary"
+              size="small"
+              outlined
+            />
             <Button label="Sign Up" size="small" />
           </div>
         </div>
       </header>
     </>
-  )
-}
+  );
+};
 
 export default Header;
